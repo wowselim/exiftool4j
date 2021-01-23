453e4db489b4119606ef510e015689620ad8a4c5
Selim Dincer <wowselim@live.de>
2021-01-23 23:11:30 +0100

    remove timestamp from dates for 3.0.1

--- src/main/java/co/selim/exiftool4j/ExifKey.kt
+++ src/main/java/co/selim/exiftool4j/ExifKey.kt
@@ -1,15 +1,15 @@
 package co.selim.exiftool4j
 
 enum class ExifKey(internal val names: List<String>, internal val exclude: List<String> = emptyList()) {
-    DATE_TIME(listOf("Date/Time Original", "Create Date", "Time Stamp")),
-    MAKE(listOf("Make")),
-    MODEL(listOf("Model", "Camera Model Name")),
-    LENS(listOf("Lens", "Lens Model", "Lens Type", "Lens ID")),
-    APERTURE(listOf("Aperture")),
-    SHUTTER_SPEED(listOf("Shutter Speed", "Exposure Time")),
-    ISO(listOf("ISO", "Program ISO")),
-    FLASH_STATUS(listOf("Flash")),
+  DATE_TIME(listOf("Date/Time Original", "Create Date", "Digital Creation Date", "Digital Creation Date/Time")),
+  MAKE(listOf("Make")),
+  MODEL(listOf("Model", "Camera Model Name")),
+  LENS(listOf("Lens", "Lens Model", "Lens Type", "Lens ID")),
+  APERTURE(listOf("Aperture")),
+  SHUTTER_SPEED(listOf("Shutter Speed", "Exposure Time")),
+  ISO(listOf("ISO", "Program ISO")),
+  FLASH_STATUS(listOf("Flash")),
 
-    // exclude equivalent focal lengths
-    FOCAL_LENGTH(listOf("Focal Length"), exclude = listOf("equivalent"))
+  // exclude equivalent focal lengths
+  FOCAL_LENGTH(listOf("Focal Length"), exclude = listOf("equivalent"))
 }
