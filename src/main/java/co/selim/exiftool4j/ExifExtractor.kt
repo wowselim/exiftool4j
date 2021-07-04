fa70208e85f8ee5c88b7742c4dec488c4586e8cd
Selim Dincer <wowselim@live.de>
2021-07-05 00:36:24 +0200

    reduce allocations & cleanup for 3.0.2

--- src/main/java/co/selim/exiftool4j/ExifExtractor.kt
+++ src/main/java/co/selim/exiftool4j/ExifExtractor.kt
@@ -2,42 +2,38 @@ package co.selim.exiftool4j
 
 import java.io.InputStream
 
-private val command = listOf("exiftool", "-")
-
 fun InputStream.extractExifData(): ExifData {
-    val exiftoolProcess = ProcessBuilder(command)
-            .redirectError(ProcessBuilder.Redirect.INHERIT)
-            .start()
+    val exiftoolProcess = ProcessBuilder("exiftool", "-")
+        .redirectError(ProcessBuilder.Redirect.INHERIT)
+        .start()
 
     exiftoolProcess.outputStream.use {
         this.copyTo(it)
     }
 
-    val output = exiftoolProcess.inputStream.reader().use {
-        it.readText()
-    }
-
-    val fields = extractKeyValuePairs(output)
-            .mapNotNull { (key, value) ->
-                getExifKey(key)?.let { exifKey ->
-                    if (exifKey.exclude.none { value.contains(it, true) })
-                        exifKey to value
-                    else
-                        null
-                }
-            }.toMap()
+    val fields = exiftoolProcess.inputStream
+        .bufferedReader()
+        .useLines(Sequence<String>::toExifMap)
 
     return ExifData(fields)
 }
 
-private fun extractKeyValuePairs(input: String): Sequence<Pair<String, String>> = input.lineSequence()
-        .map {
-            listOf(
-                    it.substringBefore(":").trim(),
-                    it.substringAfter(":").trim()
-            )
-        }
-        .map { it[0] to it[1] }
+private fun Sequence<String>.toExifMap(): Map<ExifKey, String> = associateNotNull { line ->
+    val key = line.substringBefore(":").trim()
+    val value = line.substringAfter(":").trim()
+    val exifKey = getExifKey(key)
+
+    if (exifKey != null && exifKey.exclude.none { value.contains(it, true) }) {
+        exifKey to value
+    } else {
+        null
+    }
+}
+
+private fun <T, K, V> Sequence<T>.associateNotNull(transform: (T) -> Pair<K, V>?): Map<K, V> {
+    return mapNotNull(transform).toMap()
+}
+
 
 private fun getExifKey(key: String): ExifKey? = ExifKey.values()
-        .firstOrNull { it.names.contains(key) }
+    .firstOrNull { it.names.any { name -> name.equals(key, true) } }
